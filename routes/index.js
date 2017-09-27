732d074dc3ccb1683be6bccadbfacbcebd060ff9
Oleksandr Zhuravlov <rapidexact@gmail.com>
2017-09-27 23:05:45 +0300

    new design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tap' });
+  res.render('index', { title: 'TapChallenge.io' });
 });
 
 module.exports = router;
