23ecfece02899a533f03b5939835791a050041b0
Oleksandr Zhuravlov <rapidexact@gmail.com>
2017-09-29 20:28:45 +0300

    added non www redirection

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', function (req, res, next) {
+    if (req.headers.host.match(/^www/) !== null ) {
+        res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+    } else {
+        next();
+    }
+});
 app.use('/', index);
 app.use('/users', users);
 
