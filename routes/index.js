8923889b9794c2c389f429f086619aff2448ae53
Oleksandr Zhuravlov <rapidexact@gmail.com>
2017-10-12 23:50:27 +0300

    vk call module added

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ const config = require('config');
 const dbConfig = config.get('dbConfig');
 const Sequelize = require('sequelize');
 const crypto = require('crypto');
+const request = require('request');
 const _ = require('underscore');
+const vk = require('vk-call').vk;
 
 const sequelize = new Sequelize(dbConfig.connectUrl);
 
 const Records = sequelize.import(__dirname + "/../models/records");
+
+const BASE_OPEN_API_URL = 'https://api.vk.com/';
 
 Records.sync().then(() => {
     console.log('records table created');
@@ -34,13 +38,35 @@ function parseOpenApiCookie(cookie) {
     });
     return result;
 }
+function openApiCall(methodName, options, token, apiVersion) {
+    request.get({url:BASE_OPEN_API_URL + `method/${methodName}`,
+    qs: {
+        user_id: 413999592,
+        access_token: token,
+        v: apiVersion
+    }}).on('response', function (response) {
+        return response;
+    });
+
+}
 router.use(function (req, res, next) {
     if (req.cookies[openApiCookieName]) {
         let session = parseOpenApiCookie(req.cookies[openApiCookieName]);
         console.log(isTokenValid(session, creds.protectedAppKey) ? 'User token is valid' : 'User token is invalid');
         req.user = isTokenValid(session, creds.protectedAppKey);
     }
-    next();
+
+
+    var api = new vk({
+        token: '785b787c785b787c785b787c947805ab147785b785b787c2180ac8b63dc09f0341d6154',
+        version: "5.50",
+        timeout: 10000
+    });
+
+    api.call('users.get', {user_id: 413999592}).then(response=>{
+        res.json(response);
+    });
+    // next();
 });
 
 /* GET home page. */
