3ef9b661dc2a6f14c7ed8f3e6478ae0797f48330
Oleksandr Zhuravlov <rapidexact@gmail.com>
2017-11-02 08:35:31 +0200

    v

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
